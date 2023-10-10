f3065b3536922d78ca0deedd1c8b297f53b54da2
Restyled.io <commits@restyled.io>
2023-10-10 01:33:44 +0000

    Restyled by autopep8

--- ICPCExercises/MaxSellStock/MaxSellStock.py
+++ ICPCExercises/MaxSellStock/MaxSellStock.py
@@ -16,25 +16,20 @@ Explanation: In this case, no transactions are done and the max profit = 0.
 '''
 # prices = input()
 # prices = [x for x in prices.split(' ')]
-prices = [7,1,5,3,6,4]
+prices = [7, 1, 5, 3, 6, 4]
 # prices = [7,6,4,3,1]
 curr_idx = 0
 max_sell = 0
 min = prices[curr_idx]
 for idx, price in enumerate(prices[curr_idx:]):
-     if idx < curr_idx:
-          continue
-     else:
-          if price < min:
-               min = price
-               curr_idx = idx
-          else:
-               if (price - min) > max_sell:
-                    max_sell = price - min
+    if idx < curr_idx:
+        continue
+    else:
+        if price < min:
+            min = price
+            curr_idx = idx
+        else:
+            if (price - min) > max_sell:
+                max_sell = price - min
 
 print(max_sell)
-
-     
-     
-     
-     

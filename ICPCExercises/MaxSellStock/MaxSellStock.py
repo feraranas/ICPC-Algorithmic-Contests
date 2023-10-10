c3f1c976ca3b5f100348a350b631e427d452d7a1
Restyled.io <commits@restyled.io>
2023-10-10 01:33:45 +0000

    Restyled by black

--- ICPCExercises/MaxSellStock/MaxSellStock.py
+++ ICPCExercises/MaxSellStock/MaxSellStock.py
@@ -1,4 +1,4 @@
-'''
+"""
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
@@ -13,7 +13,7 @@ Example 2:
 Input: prices = [7,6,4,3,1]
 Output: 0
 Explanation: In this case, no transactions are done and the max profit = 0.
-'''
+"""
 # prices = input()
 # prices = [x for x in prices.split(' ')]
 prices = [7, 1, 5, 3, 6, 4]

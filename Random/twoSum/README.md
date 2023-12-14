80070a747cd1f8e6dfae11efd86be6e5efd99215
feraranas <46a12sFAASfaas>
2023-12-14 12:23:49 -0600

    two sum optimized

--- Random/twoSum/README.md
+++ Random/twoSum/README.md
@@ -35,3 +35,21 @@ You can return the answer in any order.
 - -109 <= nums[i] <= 109
 - -109 <= target <= 109
 - Only one valid answer exists.
+
+<h3>Pseudocode</h3>
+
+- Initialize an empty dictionary (d)
+- Iterate through the array
+- For each number in array, calculate its complement (target - number)
+- If complement (value) in dictionary, return the key and the index of current number
+- Else add key->(current index) and value->(currentnumber) to the dictionary (d)
+
+<h3>Complexity</h3>
+
+The time complexity of the twoSum function is O(n), where n is the number of elements in the nums list. Let's break down the reasoning:
+
+The function iterates through each element in the nums list exactly once using a for loop. This contributes O(n) to the time complexity.
+
+Inside the loop, the function calls the find_key function, which iterates through the items of the dictionary (d) to find a key with a specific value. In the worst case, this operation can be O(n), where n is the number of elements in the dictionary. However, since the dictionary is built incrementally and only contains elements encountered so far, on average, the number of elements in the dictionary will be much smaller than n. Therefore, the overall time complexity of the find_key function can be considered closer to O(1) on average.
+
+Considering the loop dominates the time complexity, and the find_key function is closer to O(1) on average, the overall time complexity of the twoSum function is O(n).
--- Random/twoSum/twoSum.py
+++ Random/twoSum/twoSum.py
@@ -1,12 +1,42 @@
 from typing import List
 
 # Brute force
-class Solution:
-    def twoSum(nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if (nums[i] + nums[j] == target):
-                    print([i, j])
+# class Solution:
+#     def twoSum(nums: List[int], target: int) -> List[int]:
+#         for i in range(len(nums)):
+#             for j in range(i + 1, len(nums)):
+#                 if (nums[i] + nums[j] == target):
+#                     print([i, j])
 
-print(Solution.twoSum([2,7,11,15], 9)) # [0,1]
-print(Solution.twoSum([3,2,4], 6)) # [1,2]
+# Optimized
+
+def find_key(input_dict, value):
+   return next((k for k, v in input_dict.items() if v == value), None)
+
+# Optimum Speed
+def twoSum(nums: List[int], target: int) -> List[int]:
+     d = dict()
+     for idx, i in enumerate(nums):
+          c = target - i
+          k = find_key(d, c)
+          if k != None:
+               return [k, idx]
+          else:
+               d[idx] = i
+     return []
+
+# Optimum Memory
+def twoSumOptimumMemory(nums: List[int], target: int) -> List[int]:
+   pred = {} # predicate -> i
+   for i, n in enumerate(nums):
+       p = target - n
+       if p in pred:
+           return [pred[p], i]
+       pred[n] = i
+
+   return []
+
+print(twoSum([2,7,11,15], 9)) # [0,1]
+print(twoSum([3,2,4], 6)) # [1,2]
+print(twoSumOptimumMemory([2,7,11,15], 9)) # [0,1]
+print(twoSumOptimumMemory([3,2,4], 6)) # [1,2]

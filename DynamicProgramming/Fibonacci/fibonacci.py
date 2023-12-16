8d1489195f60f593a209f3d1feceb5166706bab1
Restyled.io <commits@restyled.io>
2023-12-16 05:56:48 +0000

    Restyled by black

--- DynamicProgramming/Fibonacci/fibonacci.py
+++ DynamicProgramming/Fibonacci/fibonacci.py
@@ -5,6 +5,7 @@ def fibonacci(n):
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
 
+
 # print(fibonacci(3)) # 2
 # print(fibonacci(4)) # 3
 # print(fibonacci(5)) # 5
@@ -22,6 +23,7 @@ def fibonacci_memo(n, memo={}):
     elif n <= 2:
         return 1
     return fibonacci(n - 1) + fibonacci(n - 2)
+
 
 # print(fibonacci(3)) # 2
 # print(fibonacci(4)) # 3

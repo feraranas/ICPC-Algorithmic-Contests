3a7c490ffe92fd26943e7fcae4e922c43c0adbcd
Restyled.io <commits@restyled.io>
2023-12-16 05:56:43 +0000

    Restyled by autopep8

--- DynamicProgramming/Fibonacci/fibonacci.py
+++ DynamicProgramming/Fibonacci/fibonacci.py
@@ -1,9 +1,9 @@
 def fibonacci(n):
-     if n == 0:
-          return 0
-     elif n <= 2:
-          return 1
-     return fibonacci(n - 1) + fibonacci(n - 2)
+    if n == 0:
+        return 0
+    elif n <= 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 # print(fibonacci(3)) # 2
 # print(fibonacci(4)) # 3
@@ -13,14 +13,15 @@ def fibonacci(n):
 # print(fibonacci(8)) # 21
 # print(fibonacci(9)) # 34
 
-def fibonacci_memo(n, memo = {}):
-     if memo.get(n):
-          return memo[n]
-     if n == 0:
-          return 0
-     elif n <= 2:
-          return 1
-     return fibonacci(n - 1) + fibonacci(n - 2)
+
+def fibonacci_memo(n, memo={}):
+    if memo.get(n):
+        return memo[n]
+    if n == 0:
+        return 0
+    elif n <= 2:
+        return 1
+    return fibonacci(n - 1) + fibonacci(n - 2)
 
 # print(fibonacci(3)) # 2
 # print(fibonacci(4)) # 3

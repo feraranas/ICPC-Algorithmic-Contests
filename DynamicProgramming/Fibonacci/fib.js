d0fec971e77cdc7c583c103054fd5b72b6e22833
Restyled.io <commits@restyled.io>
2023-12-16 05:56:49 +0000

    Restyled by clang-format

--- DynamicProgramming/Fibonacci/fib.js
+++ DynamicProgramming/Fibonacci/fib.js
@@ -2,14 +2,15 @@
 
 1. Make it work (brute force). (Look for correctness)
      -> Visualize the problem as a tree.
-     -> Visualize smaller instances of the problem (shrink the size of the problem).
+     -> Visualize smaller instances of the problem (shrink the size of the
+problem).
      -> Implement the tree using recursion.
           -> Finding the base(s) case(s)
      -> Test it.
 
-2. Make it efficient. 
+2. Make it efficient.
      -> Add a memo object. (Key's and Argument (return values)).
-     -> Add a new base case to return memo values. 
+     -> Add a new base case to return memo values.
      -> Store return values into the memo.
 
 1. & 2
@@ -20,9 +21,11 @@ O(2^n) time
 O(n) space
 */
 
-const fib = (n) => {
-     if (n <= 2) return 1;
-     return fib(n - 1) + fib(n - 2);
-}
+const fib =
+    (n) => {
+      if (n <= 2)
+        return 1;
+      return fib(n - 1) + fib(n - 2);
+    }
 
-console.log(fib(13));
+           console.log(fib(13));
